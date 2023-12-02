0f4bdd4192332c5661335478698aca333086bbe1
lfCovas <luiz.covas@sou.inteli.edu.br>
2023-12-01 22:03:03 -0300

    fix: Diretoria

--- diretoria/diretoria_entity.py
+++ diretoria/diretoria_entity.py
@@ -1,4 +1,4 @@
-from diretoria import db
+from extensions import db
 
 class Diretoria(db.Model):
     nome = db.Column(db.String(255), primary_key=True)
@@ -9,7 +9,7 @@ class Escola(db.Model):
     cie = db.Column(db.String(255), primary_key=True)
     nome_diretoria = db.Column(db.String(255), db.ForeignKey('diretoria.nome'))
     nome = db.Column(db.String(255))
-    logradouro = db.Column(db.String(255))
+    endereco = db.Column(db.String(255))
     numero = db.Column(db.String(255))
     bairro = db.Column(db.String(255))
     cep = db.Column(db.String(255))
@@ -19,21 +19,8 @@ class Escola(db.Model):
             'cie': self.cie,
             'nome_diretoria': self.nome_diretoria,
             'nome': self.nome,
-            'logradouro': self.logradouro,
+            'endereco': self.endereco,
             'numero': self.numero,
             'bairro': self.bairro,
             'cep': self.cep
     }
-
-# class DiretoriaEntity:
-#     def __init__(self, nome, regiao, escolas):
-#         self._nome = nome
-#         self._regiao = regiao
-#         self._escolas = 
-
-#     def to_dict(self):
-#         return{
-#             'nome': self._nome,
-#             'regiao': self._regiao,
-#             'escolas':self._escolas
-# }
